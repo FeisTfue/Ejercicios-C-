63f8225d9396d245ebf5dbca5385f6fcd0b22065
FeisTfue <77622862+FeisTfue@users.noreply.github.com>
2021-06-06 23:54:58 -0500

    Ejercicio 3

--- Program.cs
+++ Program.cs
@@ -5,28 +5,41 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 
-namespace E3
+namespace E4
 {
     class Program
     {
         static void Main(string[] args)
         {
             Proceso oProceso = new Proceso();
-            
-            Console.WriteLine("Contando IMPARES desde un hilo: ");
+
+            Console.WriteLine("Lanzando los dos hilos al mismo tiempo: ");
             Console.WriteLine("----------------------------------------");
             Console.WriteLine();
 
-            Thread HiloImpar = new Thread(oProceso.ContarNumerosImpares);
-            HiloImpar.IsBackground = true;
-            HiloImpar.Start();
-            HiloImpar.Join();
+            Thread nuevoHiloPar = new Thread(oProceso.ContarNumerosPares);
+            Thread nuevoHiloImpar = new Thread(oProceso.ContarNumerosImpares);
+
+            nuevoHiloPar.Start();
+            nuevoHiloImpar.Start();
 
             Console.ReadLine();
         }
     }
     class Proceso
     {
+        public void ContarNumerosPares()
+        {
+            for (int numero = 0; numero <= 100; numero = numero + 2)
+            {
+                Console.Write(numero + " ");
+                for (int GranContador = 0; GranContador <= 20000000; GranContador++) ;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
         public void ContarNumerosImpares()
         {
             for (int numero = 1; numero <= 100; numero = numero + 2)
