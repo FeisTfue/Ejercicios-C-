2d26470b69356ab7a0b342072b221a015b19545a
FeisTfue <77622862+FeisTfue@users.noreply.github.com>
2021-06-06 23:49:14 -0500

    Ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace E2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proceso oProceso = new Proceso();
+
+            Console.WriteLine("Contando PARES desde un hilo: ");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine();
+
+            Thread HiloPar = new Thread(oProceso.ContarNumerosPares);
+            HiloPar.IsBackground = true;
+            HiloPar.Start();
+            HiloPar.Join();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Proceso
+    {
+        public void ContarNumerosPares()
+        {
+            for (int numero = 0; numero <= 100; numero = numero + 2)
+            {
+                Console.Write(numero + " ");
+                for (int GranContador = 0; GranContador <= 20000000; GranContador++) ;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
